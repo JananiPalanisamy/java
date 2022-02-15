55b447b289b9524ce774a0fb104c0ae41bcc1459
JananiPalanisamy <99713526+JananiPalanisamy@users.noreply.github.com>
2022-02-15 12:56:12 +0530

    Update RnaTranscription.java

--- exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
+++ exercises/practice/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,22 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+       // throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        char[] a= dnaStrand.ToCharArray();
+        for(int i=0;i<dnaStrand.length();i++)
+        {
+            if(a[i]=='A')
+                a[i]='U';
+            else if(a[i]=='C')
+                a[i]='G';
+            else if(a[i]=='G')
+                a[i]='C';
+            else if(a[i]=='T')
+                a[i]='A';
+        }
+        String rnaStrand = new String(a);
+        return rnaStrand;
+        
     }
 
 }
